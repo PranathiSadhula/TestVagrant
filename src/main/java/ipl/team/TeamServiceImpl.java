6cbc2be901a0ef9f913ec2fa0054c4f471115af6
PranathiSadhula <pranathisadhula@gmail.com>
2023-01-19 13:10:50 +0530

    TeamServiceImpl overrides method readJsonFile from IJSonService, countPlayersFrom and countPlayersWho from PlayerServiceImpl abstract class

--- src/main/java/ipl/team/TeamServiceImpl.java
+++ src/main/java/ipl/team/TeamServiceImpl.java
@@ -1,21 +1,63 @@
 package ipl.team;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import ipl.lib.json.objects.Team;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
-public abstract class JsonImpl implements IJsonService{
+public class TeamServiceImpl extends PlayerServiceImpl implements IJSonService {
+    public static Team teamDetails;
+
     @Override
-    public void readJsonFile(String filePath) throws IOException {
-        ObjectMapper objMapper = new ObjectMapper();
-        Team teamDetails =objMapper.readValue(new File(filePath), Team.class);
+    public void readJsonFile(String fileName) {
+        try {
+            Path resourcesDir = Paths.get("src", "test", "resources");
+            String path = resourcesDir.toAbsolutePath().toFile().getCanonicalPath();
+            ObjectMapper objMapper = new ObjectMapper();
+            String filePath = Arrays.stream(resourcesDir.toFile().list()).filter(filena -> filena.contains(fileName)).findAny().get().toString();
+            teamDetails = objMapper.readValue(new File(path + "/" + filePath), Team.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to read provided json file " + fileName);
+        }
 
     }
 
-    public abstract int countPlayersFrom(String country);
+    @Override
+    public int countPlayersFrom(String country) {
+        country = country.toLowerCase();
+        if (country.equalsIgnoreCase("foreign") || country.equalsIgnoreCase("nonindian")) {
+            return countPlayersFrom(false, "India");
+        }
+        return countPlayersFrom(true, country); //true, nonindian
+    }
 
-    public  abstract int countPlayersWho(String role);
+    public int countPlayersFrom(boolean notFromGivenCountry, String country) {
+        int count = 0;
+        if (notFromGivenCountry)
+            return (int) teamDetails.getPlayer().stream().filter(player -> player.getCountry().equalsIgnoreCase(country)).count();
+        else
+           /* for(Player eachPlayer : teamDetails.getPlayer()){
+                if(!eachPlayer.getCountry().equalsIgnoreCase(country)) count++;
+            }*/
+           return (int) teamDetails.getPlayer().stream().filter(player -> !player.getCountry().equalsIgnoreCase(country)).count();
+       // return count;
+    }
+
+    @Override
+    public int countPlayersWho(String role) {
+        return countPlayersWho(true, role);
+    }
+
+    public int countPlayersWho(boolean whoseRoleIsNot, String role) {
+        if (whoseRoleIsNot)
+            return (int) teamDetails.getPlayer().stream().filter(player -> !player.getRole().equals(role)).count();
+        else
+            return (int) teamDetails.getPlayer().stream().filter(player -> player.getRole().equals(role)).count();
+    }
 
 }
