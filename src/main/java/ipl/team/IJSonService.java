95cb7a09d1fc3001ba6736706028f3d5876bee87
PranathiSadhula <pranathisadhula@gmail.com>
2023-01-19 13:07:18 +0530

    IJSonService is an interface added to readJsonFile

--- src/main/java/ipl/team/IJSonService.java
+++ src/main/java/ipl/team/IJSonService.java
@@ -1,15 +1,12 @@
 package  ipl.team;
 
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
-public interface ITeamService {
+public interface IJSonService {
     /**
      * This method used to read the JSON file
      * @param filePath - helps to load the team details`
-     * @return type void
      * @author PranathiSadhula
-     * @throws ParserConfigurationException
      */
-    public void readJsonFile(String filePath) throws IOException ;
+    void readJsonFile(String filePath) throws IOException ;
 }
