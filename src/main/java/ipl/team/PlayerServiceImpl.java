a960aec65924a747275160deddfb6dfa133fcf34
PranathiSadhula <pranathisadhula@gmail.com>
2023-01-19 13:08:11 +0530

    PlayerServiceImpl is an abstract class added to validate team Players

--- src/main/java/ipl/team/PlayerServiceImpl.java
+++ src/main/java/ipl/team/PlayerServiceImpl.java
@@ -1,2 +1,10 @@
-package ipl.team;public class PlayerServiceImpl {
+package ipl.team;
+
+public abstract class PlayerServiceImpl {
+
+
+    public abstract int countPlayersFrom(String country);
+
+    public  abstract int countPlayersWho(String role);
+
 }
